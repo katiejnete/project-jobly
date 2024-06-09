912b5eaf6770478f5cb74bde0ef08ecb053e40c3
Kate Vong <katiejnete@gmail.com>
2024-06-09 01:02:18 -0700

    add sqlForFilterCompanies

--- helpers/sql.js
+++ helpers/sql.js
@@ -17,21 +17,31 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   };
 }
 
-function sqlForFilter(dataToFilter,jsToSql) {
+function sqlForFilterCompanies(dataToFilter) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+      `"${colName}"=$${idx + 1}`,
   );
 
-  if ()
+  // {minEmployees: 2, maxEmployees: 3} => ['num_employees >= 2 AND num_employees <= 3']
+  const ops = [];
+  if (keys.includes("minEmployees")) {
+    ops.push(`num_employees >= ${dataToFilter.minEmployees}`);
+  }
+  if (keys.includes("maxEmployees")) {
+    ops.push(`num_employees >= ${dataToFilter.maxEmployees}`);
+  }
+  if (keys.includes("minEmployees") && keys.includes("maxEmployees")) {
+    ops.join("AND ");
+  }
 
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
-    opperators: 
+    setOperators: ops
   };  
 }
 
