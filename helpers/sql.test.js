e059763878dbb2f70848f86bcdc93d9a759e764c
Kate Vong <katiejnete@gmail.com>
2024-06-08 23:26:02 -0700

    complete sql test

---
+++ helpers/sql.test.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const {sqlForPartialUpdate} = require("./sql");
+
+describe("sqlForPartialUpdate", function () {
+  test("update company with data", function () {
+    const data = {name: "miffy"};
+    const {setCols, values} = sqlForPartialUpdate(data,{
+        numEmployees: "num_employees",
+        logoUrl: "logo_url",
+      });
+    expect(setCols).toEqual(`"${Object.keys(data)[0]}"=$1`);
+    expect(values).toEqual(Object.values(data));
+  });
+  test("update user with data", function () {
+    const data = {username: "miffy"};
+    const {setCols, values} = sqlForPartialUpdate(data,{
+        numEmployees: "num_employees",
+        logoUrl: "logo_url",
+      });
+    expect(setCols).toEqual(`"${Object.keys(data)[0]}"=$1`);
+    expect(values).toEqual(Object.values(data));
+  });
+  test("update without data", function () {
+    try {
+        const data = {};
+        sqlForPartialUpdate(data,{
+            numEmployees: "num_employees",
+            logoUrl: "logo_url",
+          });
+    } catch (err) {
+        expect(err.message).toBe("No data");
+    }
+  });
+});
