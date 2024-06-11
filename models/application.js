635fe677142f8787e8d57be6acb5b4f3d50e6518
Kate Vong <katiejnete@gmail.com>
2024-06-10 17:36:59 -0700

    add check for existing job to Application model

--- models/application.js
+++ models/application.js
@@ -2,6 +2,7 @@
 
 const db = require("../db");
 const { NotFoundError } = require("../expressError");
+const Job = require("./job");
 
 /** Related functions for applications. */
 
@@ -15,15 +16,15 @@ class Application {
    * */
 
   static async create(username, jobId) {
-
-      const result = await db.query(
-        `INSERT INTO applications
+    await Job.get(jobId);
+    const result = await db.query(
+      `INSERT INTO applications
         (username, job_id)
         VALUES ($1, $2) RETURNING job_id AS "jobId"`,
-        [username, jobId]
-      );
-      const application = result.rows[0];
-      if (!application) throw new NotFoundError(`No job id: ${jobId}`);
+      [username, jobId]
+    );
+    const application = result.rows[0];
+    if (!application) throw new NotFoundError(`No job id: ${jobId}`);
   }
 }
 
